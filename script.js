59709b1a21cc2e49e37bbee44a68c857cc76a1cb
HARSH GOYAL <140664216+harsh-goyal-004@users.noreply.github.com>
2024-07-22 15:09:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let boxes = document.querySelectorAll(".box");
+let newGame = document.querySelector(".game-btn1");
+let reset = document.querySelector(".game-btn2");
+let divX = document.querySelector("#divX");
+let divO = document.querySelector("#divO");
+let gameGrid = document.querySelector("#game-grid");
+let turn0 = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.innerText === "") {
+      if (turn0) {
+        box.innerText = "O";
+        turn0 = false;
+      } else {
+        box.innerText = "X";
+        turn0 = true;
+      }
+
+      checkWinner();
+    }
+  });
+});
+
+const checkWinner = () => {
+  for (let pattern of winPatterns) {
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if (pos1 != "" && pos2 != "" && pos3 != "") {
+      if (pos1 === pos2 && pos1 === pos3) {
+        if (pos1 === "X") {
+          divX.classList.remove("hide");
+          gameGrid.classList.add("transparent");
+        } else {
+          divO.classList.remove("hide");
+          gameGrid.classList.add("transparent");
+        }
+      }
+    }
+  }
+};
+
+newGame.addEventListener("click", () => {
+  turn0 = true;
+  boxes.forEach((box) => {
+    if (box.innerText !== "") {
+      box.innerText = "";
+      gameGrid.classList.remove("transparent");
+      divO.classList.add("hide");
+      divX.classList.add("hide");
+    }
+  });
+});
+
+reset.addEventListener("click", () => {
+  turn0 = true;
+  boxes.forEach((box) => {
+    if (box.innerText !== "") {
+      box.innerText = "";
+      gameGrid.classList.remove("transparent");
+      divO.classList.add("hide");
+      divX.classList.add("hide");
+    }
+  });
+});
